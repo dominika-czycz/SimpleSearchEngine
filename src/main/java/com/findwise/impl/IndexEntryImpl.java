b4c0ec841a571ab229590e793975f6cf373b24b8
dominika-czycz <dominika.czycz@gmail.com>
2023-01-14 11:12:28 +0100

    adding functional requirements

---
+++ src/main/java/com/findwise/impl/IndexEntryImpl.java
@@ -0,0 +1,25 @@
+package com.findwise.impl;
+
+import com.findwise.IndexEntry;
+
+public class IndexEntryImpl implements IndexEntry {
+    @Override
+    public String getId() {
+        return null;
+    }
+
+    @Override
+    public void setId(String id) {
+
+    }
+
+    @Override
+    public double getScore() {
+        return 0;
+    }
+
+    @Override
+    public void setScore(double score) {
+
+    }
+}
---
+++ src/main/java/com/findwise/impl/SearchEngineImpl.java
@@ -0,0 +1,18 @@
+package com.findwise.impl;
+
+import com.findwise.IndexEntry;
+import com.findwise.SearchEngine;
+
+import java.util.List;
+
+public class SearchEngineImpl implements SearchEngine {
+    @Override
+    public void indexDocument(String id, String content) {
+
+    }
+
+    @Override
+    public List<IndexEntry> search(String term) {
+        return null;
+    }
+}
